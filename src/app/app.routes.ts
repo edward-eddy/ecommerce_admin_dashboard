eb2388a99bfca262bd5a916a940cb018a21c2632
Edward Sameh <eddysameh@yahoo.com>
2023-11-19 17:13:20 +0200

    tailwind css setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './Pages/Dashboard/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path:"", component:DashboardComponent, title:"Admin Dashboard"}
+];
